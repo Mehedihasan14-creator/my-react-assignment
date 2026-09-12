486b0ccc285b98733a7cf74a219b6bc1f3302574
Mehedihasan14-creator <hasancoc77@gmail.com>
2026-09-12 08:20:32 +0600

    Navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,16 @@ const Navbar=()=>{
     return (
         <div className="border-b border-gray-300"> 
          <nav className="flex justify-between gap-4 container mx-auto py-4">
-            <img src={Logo} className="w-[120px] h-[30px]" alt="" />
+            <img src={Logo} className="w-[120px] h-[40px]" alt="" />
             <ul className="flex gap-4 items-center">
-                <li><a href="/">Home </a></li>
+                <li className="text-pink-500"><a href="/">Home </a></li>
                 <li><a href="/">Technogies </a></li>
                 <li><a href="/">Projects</a></li>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Contact</a></li>
             </ul>
-            <div>
-                <button>Sign In</button>
+            <div className="flex gap-4">
+                <button className="">Sign In</button>
                 <button className="bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-black">Sign Up</button>
             </div>
            </nav>
